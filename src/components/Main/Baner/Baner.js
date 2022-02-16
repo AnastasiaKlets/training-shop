bf4f05fbb703c7860cece4d1026f58ab604ab7a0
klecz.anastasia@gmail.com <klecz.anastasia@gmail.com>
2022-02-16 11:02:08 +0300

    border 1.1

--- src/components/Main/Baner/Baner.js
+++ src/components/Main/Baner/Baner.js
@@ -1,42 +1,71 @@
 import React from 'react';
+import img1 from "../../../img/img1.svg";
+import img2 from "../../../img/img2.svg";
+import img3 from "../../../img/img3.svg";
+import img4 from "../../../img/img4.svg";
+import './Baner.css';
 
 function Baner(props) {
     return (
-        <div>
-            <div>   
-                <div>img1</div>
-                <div>
-                    <div>
-                        <div>img2</div>
-                        <div>img3</div>
-                    </div>
-                    <div>img 4</div>  
-                </div>
-            </div> 
-           <div>
-                <div>
-                    <div>img5</div>
-                    <div>
-                        <div>titl</div>
-                        <div>dis</div>
-                    </div>
-                </div>
-                <div>
-                    <div>img6</div>
-                    <div>
-                        <div>titl</div>
-                        <div>dis</div>
-                    </div>
-                </div>
-                <div>
-                    <div>img7   </div>
-                    <div>
-                        <div>titl</div>
-                        <div>dis</div>
-                    </div>
-                </div>
-           </div>
-        </div>
+        <div className="container">
+			<section className="content">
+				<div className="contentItems">
+					<div className="contentItem contentItemBig">
+						<div className="contentItemsImage">
+							{/* <img src={img1} alt="img1" /> */}
+							<div className="slider">
+								<div className="contentSlider">
+									<div className="rightControl sliderControl">
+										<div className="arrow left"></div>
+									</div>									
+									<div className="SliderContent">
+										<div className="title">BUNNER</div>
+										<div className="text">YOUR TITLE TEXT</div>
+									</div>
+									<div className="leftControl sliderControl">
+										<div className="arrow right"></div>
+									</div>									
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="contentItem small_item_1">
+					
+								<div className="columnLittle">
+									<div className="columnImage">
+										<img src={img2} alt="img2" />
+										<div className="blockLink">
+											<a href="/" className="link">Woman</a>
+										</div>
+									</div>
+								</div>
+								
+							
+					</div>
+					<div className="contentItem small_item_2">
+						
+								<div className="columnLittle">
+									<div className="columnImage">
+										<img src={img3} alt="img3" />
+										<div className="blockLink">
+											<a href="/" className="link">Men</a>
+										</div>
+									</div>
+								
+						</div>
+					</div>
+					<div className="contentItem avg_item">
+							<div className="middle">
+								<img src={img4} alt="img4" />
+								<div className="blockLink">
+									<a href="/" className="link">Accessories</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+			</section>
+		</div>
     );
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import Baner from './Baner/Baner'
+import Baner from './Baner/Baner';
+import InfAfterBaner from './InfAfterBaner/InfAfterBaner';
+import WomenPart from './WomenPart/WomenPart';
+import MenPart from './MenPart/MenPart';
+import InfOfColection from './InfOfCollection/InfOfCollection';
+import Sales from './Sales/Sales';
+import AnotherInf from './AnotherInf/AnotherInf';
+
 
 function Main(props) {
     return (
         <>
-         <Baner/>   
+            <Baner/>           
+
         </>
     );
 }
